bf30a8f765cf1955c6748d2336d6c279c40f8d9b
vivek <vivekshivam007@gmail.com>
2018-06-06 20:10:53 +0530

    assignment of intrduction to python

---
+++ assignment2.py
@@ -0,0 +1,42 @@
+
+
+#ques 1
+
+print("hello world")
+
+
+#ques2
+
+print("Vivek"+"Shivam")
+
+
+#ques3
+
+x=input("enter the value of x")
+y=input("enter the value of y")
+z=input("enter the value of z")
+print("X=",x)
+print("Y=",y)
+print("Z=",z)
+
+
+
+
+
+#ques4
+
+print("Let's get started")
+
+
+#ques5
+
+s = "Acadview"
+course = "Python"
+fees = 5000
+print(("the fees of '%s' in '%s' is '%d'") % (course, s , fees))
+
+
+#ques6
+name = "acadview"
+salary = 100000
+print(('%s''%d')%(name,salary))
